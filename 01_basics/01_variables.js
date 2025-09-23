681a73790f5d060ded346ce33f73d86a88623553
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-09-23 11:59:04 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=24355
+let accountEmail='shubh@gmail.com'
+var accountPassword='13646648'
+accountCity='jaipur'
+// accountId=2 not allowed 
+console.log(accountId)
+accountPassword='iru8477'
+accountEmail='ram@gmail.com'
+accountCity='ramghar'
+console.table([accountId,accountEmail,accountPassword,accountCity])
+let accountState;
+let accountpy=''
+console.log(accountState)
+console.log(accountpy)
+
+/*
+prefer not to use var
+because of issue in block scope 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version...!!
+// alert(3 + 3); we are using node js not browser....!!
+
+let name='shubham'; //string
+let age=23;//number
+let islogged=false //boolean
+let city=null; //null
+
+/*
+PRIMITIVE DATATYPES...
+number datatypes
+string datatypes
+boolean datatypes
+null datatypes(standalone value)
+undefine datatypes
+symbol datatypes (unique)
+*/
+
+//OBJECTS.......
+
+// console.log(typeof 'shubham');
+// console.log(typeof age);
+// console.log(typeof null);
+console.log(typeof undefined);
+
+
