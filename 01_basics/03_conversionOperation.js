ef2ae235f007bbd9afeb08107ed01a851afee705
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-09-23 12:24:15 +0000

    datatypes conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+// let  score=33;
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let score='33abb'
+// let score=null;
+// let score=true
+// let score=undefined
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+/*
+'33'=> 33
+'33abc'=>NaN
+null=>0
+undefined=>NaN
+true=> 1
+false=>0
+*/
+
+
+//boolean
+
+// let isLogged='fhhdfhej'
+// let isBoolean=Boolean(isLogged)
+// console.log(isBoolean);
+// console.log(typeof(isBoolean));
+
+/* 
+1 => true
+0 => false
+'' => false
+'ghhs'=>true
+*/
+
+
+//string
+
+let someNumber=46
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
