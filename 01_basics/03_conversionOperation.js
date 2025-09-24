5d1ea40c71ddcae430e0c3076f631cab61a76176
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-09-24 19:13:09 +0000

    change in location of file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,7 +39,82 @@ false=>0
 
 //string
 
-let someNumber=46
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=46
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//operations*************
+
+let value=3
+let negativeValue=-value
+// console.log(negativeValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+str1='hello'
+str2=' shubham'
+// console.log(str1+str2)
+
+// console.log('1'+2); //12
+// console.log(1+'2'); //12
+// console.log('1'+'2'); //12
+// console.log('1'+2+2); //112
+// console.log(1+2+'2'); //32
+
+// console.log(true); true
+// console.log(+true); 1 
+// console.log(+''); 0
+
+score=100
+++score
+score++
+// console.log(score);
+
+
+//comparision of datatypes**********
+
+// console.log(2>1); true
+// console.log(2<=1); false
+// console.log(2<1); false
+// console.log(2==2); true
+// console.log(2!=2);  false
+
+
+// console.log('2'>1); true
+// console.log('02'>1);true
+
+// console.log(null>0);false
+// console.log(null==0);false
+// console.log(null>=0); true
+// console.log('2'==2); true
+// console.log(undefined>0);false
+// console.log(undefined<0);false
+// console.log(undefined==0);false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ datatypes-summury.js
@@ -0,0 +1,25 @@
+//primitive(7 categories...) it is call by value..
+
+// 1>String (const name='shubham')
+// 2>Number 100,() const age=100.3)
+// 3>Boolean (const isloggedin=true/false)
+// 4>null (const outsideTemp=null)
+// 5>undefined (const useremail;)
+// 6>Symbol (const id=Symbol('123') / const anotherId=symbol('123') this is not same
+// 7>BigInt (const bignumber=26364646623n)
+
+//Reference (non primitive)..........
+
+// 1>Array (const heros=['saktiman','spiderman','antman'])
+// 2>Object (const myob{'name':'hitesh','age':22}) it can different datatypes..
+// 3>Functions const myfunction=function(){console.log('hello world')}
+
+
+// datatype pata karne ka easy tareeka 
+
+// console.log(typeof);
+
+
+
+
+
